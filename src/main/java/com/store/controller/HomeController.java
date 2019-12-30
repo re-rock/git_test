bd6d2d3cc6ec0336b1034714133bb82a2712a2e3
myuser <baysik734@gmail.com>
2019-12-30 09:43:24 +0900

    コメント追加

--- src/main/java/com/store/controller/HomeController.java
+++ src/main/java/com/store/controller/HomeController.java
@@ -13,6 +13,7 @@ public class HomeController {
     @GetMapping("/home")
     public String getHome(Model model) {
 
+        // modelにフラグメントを登録する
         model.addAttribute("contents", "home :: home_contents");
 
         return "base";
